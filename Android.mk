4abc950e2f07735dbcd28d92f03c11e6205dd1b3
Tuan Anh <tuan73176@gmail.com>
2026-03-31 04:33:54 +0000

    TA-1041_00CN-user 9 PPR1.180610.011 00CN_3_40C release-keys
    Change-Id: I503d3b539648658aa45cc546d22dcbbb97f2f88c

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),C1N)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,a93d5183866ede0b9ec624213cfb17bbca83a87c)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,4999b2497ea376d58c22186e2900777fc7f668ef)
+$(call add-radio-file-sha1-checked,radio/cda.img,cd416efb5f260e1f49aef576f804a3c5799821ae)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,440cb14b4d2a001879ea97e9250ae237a45c2021)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,5afd6403e9a03258612a00b0f2d9c49dca883da6)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,487c9b5f8ad454b77b0571cec99a63b6c662b5cf)
+$(call add-radio-file-sha1-checked,radio/dsp.img,81eebc7dc33988c3e99e66c91de592aefc448b2c)
+$(call add-radio-file-sha1-checked,radio/hidden.img,9f7103658b0209b74137d6cb7e39986e54b03c04)
+$(call add-radio-file-sha1-checked,radio/hyp.img,e42a102323f853559898e9f320c1fbcc65b2b8d8)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,922929a6689c4d962e84409b36bfb507b7a61c8a)
+$(call add-radio-file-sha1-checked,radio/mdtp.img,b186389906aa96087fe3b6e0f2bcc1d4054bd02d)
+$(call add-radio-file-sha1-checked,radio/mdtpsecapp.img,216fdfbe04d1cdc4d9533ef8c9e9a31840ae511a)
+$(call add-radio-file-sha1-checked,radio/modem.img,379d4dfa025296e4f73eadc1996985646ef498d2)
+$(call add-radio-file-sha1-checked,radio/nvdef.img,654dbd20074b6e4b0858b20cd95533a25a3f2732)
+$(call add-radio-file-sha1-checked,radio/pmic.img,a72407d16797b1fdca1c67e412e4f3018598a69c)
+$(call add-radio-file-sha1-checked,radio/rpm.img,dee0471341613153a42829cacbdbe31594335bc3)
+$(call add-radio-file-sha1-checked,radio/splash.img,6c8843912cbd5661c2c25d5d0d12ace94537f5f4)
+$(call add-radio-file-sha1-checked,radio/systeminfo.img,42dd116b8accec46234cf645d4a24427b8dbf63e)
+$(call add-radio-file-sha1-checked,radio/tz.img,cc2a639e59565832449896623ace35d0ab7bb5a4)
+$(call add-radio-file-sha1-checked,radio/xbl.img,01186309fb79d4f01f1315f04de25919f6edbe5c)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,25 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    cda \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    hidden \
+    hyp \
+    keymaster \
+    mdtp \
+    mdtpsecapp \
+    modem \
+    nvdef \
+    pmic \
+    rpm \
+    splash \
+    systeminfo \
+    tz \
+    xbl
